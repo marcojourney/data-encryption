23e441ada07f2246a642a990989238fc61f53033
Marco <mornsophannamis@gmail.com>
2023-08-08 14:40:46 +0700

    ignore folder node_modules

---
+++ aes.js
@@ -0,0 +1,32 @@
+//Advanced Encryption Standard (AES): AES is a symmetric encryption algorithm that is considered to be very secure. It is used by many different organizations, including the US government.
+'use strict';
+const CryptoJS = require('crypto-js');
+
+module.exports = {
+    aesEncrypt: aesEncrypt,
+    aesDecrypt: aesDecrypt
+};
+
+const secret_key = 'this_is_a_secret_key';
+const your_secret_iv = 'this_is_an_iv';
+
+function aesEncrypt(content) {
+    const parsedkey = CryptoJS.enc.Utf8.parse(secret_key);
+    const iv = CryptoJS.enc.Utf8.parse(your_secret_iv);
+    const encrypted = CryptoJS.AES.encrypt(content, parsedkey, { iv: iv, mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
+    return encrypted.toString();
+};
+
+function aesDecrypt(word) {
+    var keys = CryptoJS.enc.Utf8.parse(secret_key);
+    let base64 = CryptoJS.enc.Base64.parse(word);
+    let src = CryptoJS.enc.Base64.stringify(base64);
+    var decrypt = CryptoJS.AES.decrypt(src, keys, { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.Pkcs7 });
+    return decrypt.toString(CryptoJS.enc.Utf8);
+};
+
+const bankAcccNo = '123456789';
+
+const encryptedBankAcc = aesEncrypt(bankAcccNo);
+console.log('Encrypted Bank Acc.', encryptedBankAcc);
+console.log('Decrypted Bank Acc.', aesDecrypt(encryptedBankAcc));
--- rsa-encryption.js
+++ rsa-encryption.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 
 const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
     // The length of our RSA keys is 3072 bits
-    modulusLength: 3072,
+    modulusLength: 3072
 });
 
 // console.log(publicKey.export({
@@ -27,18 +27,15 @@ const encryptedData = crypto.publicEncrypt(
     Buffer.from(bankAcccNo, 'utf-8')
 );
 
-console.log("Encrypted Bank Acc No.", encryptedData.toString("base64").length);
+console.log("Encrypted Bank Acc No.", encryptedData.toString("base64"));
 
-// const dataToEncrypt = fs.readFileSync('data_to_encrypt.txt', {encoding: 'utf-8'});
-// const publicKey = Buffer.from(fs.readFileSync('id_rsa.pub', {encoding: 'utf-8'}));
+const decryptedData = crypto.privateDecrypt(
+    {
+        key: privateKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: "sha512",
+    },
+    encryptedData
+)
 
-// const encryptedData = crypto.publicEncrypt(
-//     {
-//       key: publicKey,
-//       padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-//       oaepHash: 'sha256'
-//     },
-//     Buffer.from(dataToEncrypt)
-// )
-
-// fs.writeFileSync('encrypted_data.txt', encryptedData.toString('base64'), { encoding: 'utf-8' })
+console.log("\n\nBecrypt Bank Acc No.", decryptedData.toString());
